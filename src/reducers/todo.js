cecdc28c97f0c9046fbad7a57a7d049b460b972d
yuki.morimoto <morimoto@infodex.co.jp>
2019-01-29 18:05:30 +0900

    actionを受け取ってstateを書き換える処理をするreducerを追加

---
+++ src/reducers/todo.js
@@ -0,0 +1,16 @@
+// ここにreducerを書く
+// actionがdispatchされるとreducerにactionが流れていく
+// action.typeにかかれた文字列をswitchの条件に入れて、処理をわける
+// reducerはただの関数で、引数は2つとれる
+// 1つ目はstate, 2つ目はdispatchされたaction
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_INPUT_TODO": // actionのtypeで処理が分岐する
+      return {
+        ...state, // 「...」はスプレッド演算子といって、オブジェクトの中身を展開する
+        inputTodo: action.payload.inputTodo // /src/actios/todo.js で作ったaction creatorが返してくれるactionのpayloadのデータを利用する。今回は、入力欄に入力された文字がここに入る
+      };
+    default:
+      break;
+  }
+};
