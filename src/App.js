571dc8ced9449dd36cab92f65293e2af160f488d
yuki.morimoto <morimoto@infodex.co.jp>
2019-01-29 18:34:39 +0900

    入力されたときにstateに保存されていくようにonChangeとvalueを設定

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="App">
-        <input type="text" placeholder="TODOを入力してください" />
+        <input
+          type="text"
+          placeholder="TODOを入力してください"
+          value={this.props.inputTodo} // connectした時に渡したprops(mapStateToProps)
+          onChange={e => this.props.changeInput(e.target.value)} // connectした時に渡したprops, dispatchする関数。e.target.valueで入力欄の値が取れる
+        />
         <button>追加</button>
       </div>
     );
